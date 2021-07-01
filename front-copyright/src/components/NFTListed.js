311461a448d3200f25ad63714b154a2d95ffaacc
johnfr14 <jonathan.alyra@gmail.com>
2021-07-01 03:34:16 +0200

    go

--- front-copyright/src/components/NFTListed.js
+++ front-copyright/src/components/NFTListed.js
@@ -1,17 +1,22 @@
 import { Center, Container, Circle} from "@chakra-ui/react"
 import { Tabs, TabList, TabPanels, Tab, TabPanel} from "@chakra-ui/react"
+import { useState } from "react"
 import Tab2Listed from "./Tab2Listed"
 import Tab3 from "./Tab3"
 
 const NFTListed = ({nft}) => {
-  
+  const [tabIndex, setTabIndex] = useState(0)
+
+  const handleTabsChange = (index) => {
+    setTabIndex(index)
+  }
   return (
   <Container backgroundColor="purple.400" borderRadius="3rem" height="30rem" width="24rem" >
     <Circle mb="-8" position="relative" bottom="1rem" left="-11rem"  size="16" fontWeight="bold" fontSize="40" bg={nft.isApprove ? "gray.500" : "tomato"} color="white">
       {nft.id}
     </Circle>
 
-    <Tabs variant="soft-rounded" colorScheme="green" defaultIndex={1}>
+    <Tabs index={tabIndex} onChange={handleTabsChange} variant="soft-rounded" colorScheme="green" defaultIndex={1}>
         <TabPanels>
 
           <TabPanel >
@@ -25,7 +30,7 @@ const NFTListed = ({nft}) => {
         </TabPanels>
         <Center>
           <TabList mb="5" >
-            <Tab _selected>Tab 1</Tab>
+            <Tab defaultIndex>Tab 1</Tab>
             <Tab>info</Tab>
           </TabList>
         </Center>
